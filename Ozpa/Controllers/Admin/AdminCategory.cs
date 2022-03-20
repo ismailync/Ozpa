1a7fdc935a17d9046ea595b8678a3005dfb0b1f7
ismailync <ismailyonca2001@gmail.com>
2022-03-21 01:21:46 +0300

    AdminPanel

---
+++ Ozpa/Controllers/Admin/AdminCategory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ozpa.Controllers.Admin
+{
+    public class AdminCategory : Controller
+    {
+        public IActionResult ACategory()
+        {
+            return View();
+        }
+    }
+}
---
+++ Ozpa/Controllers/Admin/AdminNews.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ozpa.Controllers.Admin
+{
+    public class AdminNews : Controller
+    {
+        public IActionResult ANews()
+        {       
+            return View();
+        }
+    }
+}
